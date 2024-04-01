7fa2bf44e679d6b38f704cf4d80b492e21f15197
Nikolas-Pensyl <nik.pensyl@aol.com>
2024-03-31 20:25:11 -0400

    things

--- WebSockets/client_chat_w_controller.py
+++ WebSockets/client_chat_w_controller.py
@@ -18,40 +18,39 @@ joystick = pygame._sdl2.controller.Controller(0)
 joystick.init()
 
 async def send_message():
-    uri = "ws://192.168.4.1:8765"
+    uri = "ws://10.42.0.1:8765"
     async with websockets.connect(uri) as websocket:
         while True:
             # Send a message to the server
             message = ''
+            for event in pygame.event.get():
+                if event.type == pygame.CONTROLLERAXISMOTION:
+                    # Joystick axis motion event
+                    message = f"Axis {event.axis}: {joystick.get_axis(event.axis)}"
+                    break
+                
+                elif event.type == pygame.CONTROLLER_BUTTON_DPAD_DOWN:
+                    # Joystick button up event
+                    message = f"DPAD {event.DPAD} down"
+                    break
+                
+                elif event.type == pygame.CONTROLLERBUTTONDOWN:
+                    # Joystick button down event
+                    message = f"Button {event.button} down"
+                    break
 
-            while message == '':
-                for event in pygame.event.get():
-                    if event.type == pygame.CONTROLLERAXISMOTION:
-                        # Joystick axis motion event
-                        message = f"Axis {event.axis}: {joystick.get_axis(event.axis)}"
-                        break
-                    
-                    elif event.type == pygame.CONTROLLER_BUTTON_DPAD_DOWN:
-                        # Joystick button up event
-                        message = f"DPAD {event.DPAD} down"
-                        break
-                    
-                    elif event.type == pygame.CONTROLLERBUTTONDOWN:
-                        # Joystick button down event
-                        message = f"Button {event.button} down"
-                        break
-
-                    elif event.type == pygame.CONTROLLERBUTTONUP:
-                        # Joystick button up event
-                        message = f"Button {event.button} up"
-                        break
+                elif event.type == pygame.CONTROLLERBUTTONUP:
+                    # Joystick button up event
+                    message = f"Button {event.button} up"
+                    break
 
                     
             await websocket.send(message)
 
             # Receive and print the response from the server
             response = await websocket.recv()
-            print(f"Server response: {response}")
+            if response != '':
+                print(response)
 
 # Start the WebSocket client
 asyncio.get_event_loop().run_until_complete(send_message())
